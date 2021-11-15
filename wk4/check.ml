a0417d3492bb34209809d9b2ce7885fd41ff79d9
Max Heiber <max.heiber@gmail.com>
2021-11-15 15:55:57 +0000

    tuples and records

--- wk4/check.ml
+++ wk4/check.ml
@@ -2,11 +2,14 @@
 module Ctx = Map.Make (String)
 module Vars = Set.Make(String)
 
+module StrMap = Map.Make(String)
 
 type ty =
   | TyBool
   | TyArr of ty * ty
   | TyUnit
+  | TyTuple of ty list
+  | TyRecord of ty StrMap.t
 
 type expr =
   | EUnit
@@ -19,15 +22,26 @@ type expr =
   | ESeq of expr * expr
   | EAs of expr * ty
   | ELet of string * expr * expr
+  | ETuple of expr list
+  | ETupleProj of expr * int
+  | ERecord of expr StrMap.t
+  | ERecordProj of expr * string
 
 exception Type_Mismatch of (*expected*) ty * (*got*) ty
 exception Expected_Fun_Type of (*got*) ty
+exception Expected_Tuple_Type of (*got*) ty
+exception Expected_Record_Type of (*got*) ty
 exception Shadowed of string
 
 let rec show_ty = function
   | TyUnit -> "unit"
   | TyBool -> "bool"
   | TyArr (e1, e2) -> Printf.sprintf "(%s -> %s)" (show_ty e1) (show_ty e2)
+  | TyTuple tys -> Printf.sprintf "(%s)" (String.concat ", " (List.map show_ty tys))
+  | TyRecord fields ->
+    let bindings = StrMap.bindings fields in
+    Printf.sprintf "{%s}" (String.concat ", " (List.map show_field bindings))
+and show_field (name, ty) = Printf.sprintf "%s = %s" name (show_ty ty)
 
 let e_children = function
   | EUnit | ETrue | EFalse | EVar _ -> []
@@ -43,10 +57,21 @@ let e_children = function
     [e]
   | ELet(x, e1, e2) ->
     [e1; e2]
+  | ETuple es ->
+    es
+  | ETupleProj (e, int) ->
+    [e]
+  | ERecord fields ->
+    let bindings = (StrMap.bindings fields) in
+    let snd (_, v) = v in
+    List.map snd bindings
+  | ERecordProj(record, _) ->
+    [record]
 
 let validate_no_shadow e =
   let rec aux vars e = match e with
-  | EUnit | ETrue | EFalse | EIf _ | EVar _ | EApp _ | ESeq _ | EAs _ ->
+  | EUnit | ETrue | EFalse | EIf _ | EVar _ | EApp _ | ESeq _
+  | EAs _ | ETuple _ | ETupleProj _ | ERecord _ | ERecordProj _ ->
     List.iter (aux vars) (e_children e)
   | EAbs(x, _ty, _e) when Vars.mem x vars ->
     raise (Shadowed x)
@@ -107,6 +132,26 @@ let type_of e =
       let ty = aux ctx e1 in
       let ctx = Ctx.add x ty ctx in
       aux ctx e2
+    | ETuple es ->
+      let tys = List.map (fun e -> aux ctx e) es in
+      TyTuple tys
+    | ERecord fields ->
+      let bindings = StrMap.map (fun e -> aux ctx e) fields in
+      TyRecord bindings
+    | ETupleProj(e, index) ->
+      begin match aux ctx e with
+      | TyTuple tys ->
+        List.nth tys index
+      | ty ->
+        raise (Expected_Tuple_Type ty)
+      end
+    | ERecordProj(e, field) ->
+      begin match aux ctx e with
+      | TyRecord fields ->
+        StrMap.find field fields
+      | ty ->
+        raise (Expected_Record_Type ty)
+      end
   in
   validate_no_shadow e;
   aux Ctx.empty e
@@ -127,6 +172,7 @@ let check_fails_ty_mismatch e =
     | exception (Type_Mismatch(_expected, _got)) -> ()
     | ty -> failwith (Printf.sprintf "expected exception for type mismatch, got %s" (show_ty ty))
 
+let map_of_pairs pairs = StrMap.of_seq @@ List.to_seq pairs
 
 let _ =
   assert_ty ETrue TyBool;
@@ -140,3 +186,11 @@ let _ =
   assert_ty (EAs(ETrue, TyBool)) TyBool;
   check_fails_ty_mismatch (EAs(ETrue, TyUnit));
   assert_ty (ELet("x", ETrue, EVar"x")) TyBool;
+  let tuple = (ETuple [ETrue; EUnit; EFalse]) in
+  assert_ty tuple (TyTuple [TyBool; TyUnit; TyBool]);
+  assert_ty (ETupleProj(tuple, 0)) TyBool;
+  assert_ty (ETupleProj(tuple, 1)) TyUnit;
+  let record = ERecord (map_of_pairs [("a", ETrue); ("b", EUnit); ("c", EFalse)]) in
+  assert_ty record (TyRecord (map_of_pairs [("a", TyBool); ("b", TyUnit); ("c", TyBool)]));
+  assert_ty (ERecordProj(record, "a")) TyBool;
+  assert_ty (ERecordProj(record, "b")) TyUnit;
