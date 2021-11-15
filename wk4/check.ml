f53276990b8738a3645654b89b521ec7897f53c4
Max Heiber <max.heiber@gmail.com>
2021-11-15 22:12:35 +0000

    variants

--- wk4/check.ml
+++ wk4/check.ml
@@ -1,8 +1,7 @@
-
-module Ctx = Map.Make (String)
-module Vars = Set.Make(String)
-
+module StrSet = Set.Make (String)
 module StrMap = Map.Make(String)
+
+let ty_eq t1 t2 = t1 = t2
 
 type ty =
   | TyBool
@@ -10,6 +9,7 @@ type ty =
   | TyUnit
   | TyTuple of ty list
   | TyRecord of ty StrMap.t
+  | TyVariant of ty StrMap.t
 
 type expr =
   | EUnit
@@ -26,11 +26,16 @@ type expr =
   | ETupleProj of expr * int
   | ERecord of expr StrMap.t
   | ERecordProj of expr * string
+  | EVariant of (*ctor_name*) string * expr * (*variant type*)ty
+  | ECase of expr * expr StrMap.t
 
 exception Type_Mismatch of (*expected*) ty * (*got*) ty
 exception Expected_Fun_Type of (*got*) ty
 exception Expected_Tuple_Type of (*got*) ty
 exception Expected_Record_Type of (*got*) ty
+exception Expected_Variant_Type_W_Ctor of (*ctor*) (string * ty) * (*got*) ty
+exception Expected_Variant_Type_W_Ctors of StrSet.t * (*got*) ty
+exception Expected_Variant_Type of (*got*) ty
 exception Shadowed of string
 
 let rec show_ty = function
@@ -40,8 +45,11 @@ let rec show_ty = function
   | TyTuple tys -> Printf.sprintf "(%s)" (String.concat ", " (List.map show_ty tys))
   | TyRecord fields ->
     let bindings = StrMap.bindings fields in
-    Printf.sprintf "{%s}" (String.concat ", " (List.map show_field bindings))
-and show_field (name, ty) = Printf.sprintf "%s = %s" name (show_ty ty)
+    Printf.sprintf "{%s}" (String.concat ", " (List.map show_binding bindings))
+  | TyVariant ctors ->
+    let ctors = StrMap.bindings ctors in
+    Printf.sprintf "<%s>" (String.concat ", " (List.map show_binding ctors))
+and show_binding (name, ty) = Printf.sprintf "%s = %s" name (show_ty ty)
 
 let e_children = function
   | EUnit | ETrue | EFalse | EVar _ -> []
@@ -67,23 +75,29 @@ let e_children = function
     List.map snd bindings
   | ERecordProj(record, _) ->
     [record]
+  | EVariant(name, e, ty) ->
+    [e]
+  | ECase(e, handlers) ->
+    let snd (_, expr)  = expr in
+    e :: (List.map snd (StrMap.bindings handlers))
 
 let validate_no_shadow e =
   let rec aux vars e = match e with
   | EUnit | ETrue | EFalse | EIf _ | EVar _ | EApp _ | ESeq _
-  | EAs _ | ETuple _ | ETupleProj _ | ERecord _ | ERecordProj _ ->
+  | EAs _ | ETuple _ | ETupleProj _ | ERecord _ | ERecordProj _
+  | EVariant _ | ECase _ ->
     List.iter (aux vars) (e_children e)
-  | EAbs(x, _ty, _e) when Vars.mem x vars ->
+  | EAbs(x, _ty, _e) when StrSet.mem x vars ->
     raise (Shadowed x)
   | EAbs(x, _ty, e) ->
-    let vars = Vars.add x vars in
+    let vars: StrSet.t = StrSet.add x vars in
     aux vars e;
   | ELet(x, e1, e2) ->
     aux vars e1;
-    let vars = Vars.add x vars in
+    let vars = StrSet.add x vars in
     aux vars e2
   in
-  aux Vars.empty e
+  aux StrSet.empty e
 
 let type_of e =
   let rec aux ctx = function
@@ -105,14 +119,19 @@ let type_of e =
       raise (Type_Mismatch(TyBool, other))
     end
    | EVar s ->
-         Ctx.find s ctx
+         begin match StrMap.find_opt s ctx with
+         | Some ty ->
+            ty
+         | None ->
+            failwith ("unbound " ^ s)
+         end
    | EAbs(x, ty, expr) ->
-     let ctx = Ctx.add x ty ctx in
+     let ctx = StrMap.add x ty ctx in
      aux ctx expr
    | EApp(e1, e2) ->
      let t2 = aux ctx e2 in
      begin match aux ctx e1 with
-     | TyArr(domain, codomain) when domain == t2 ->
+     | TyArr(domain, codomain) when ty_eq domain t2 ->
        codomain
      | TyArr(domain, _codomain) ->
           raise (Type_Mismatch(domain, t2))
@@ -124,13 +143,13 @@ let type_of e =
       aux ctx e2
     | EAs(e, expected_ty) ->
       let actual_ty = aux ctx e in
-      if expected_ty != actual_ty then
+      if not (ty_eq expected_ty actual_ty) then
           raise (Type_Mismatch(expected_ty, actual_ty))
       else
         expected_ty
     | ELet(x, e1, e2) ->
       let ty = aux ctx e1 in
-      let ctx = Ctx.add x ty ctx in
+      let ctx = StrMap.add x ty ctx in
       aux ctx e2
     | ETuple es ->
       let tys = List.map (fun e -> aux ctx e) es in
@@ -152,9 +171,58 @@ let type_of e =
       | ty ->
         raise (Expected_Record_Type ty)
       end
+    | EVariant(ctor_name, e, variant_ty) ->
+      let ctor_ty = aux ctx e in
+      begin match variant_ty with
+      | TyVariant bindings ->
+        begin match StrMap.find_opt ctor_name bindings with
+          | Some expected_ctor_ty when ty_eq expected_ctor_ty ctor_ty ->
+            variant_ty
+          | _ ->
+            raise (Expected_Variant_Type_W_Ctor ((ctor_name, ctor_ty), variant_ty))
+        end
+      | _ ->
+        raise (Expected_Variant_Type_W_Ctor((ctor_name, ctor_ty), variant_ty))
+      end
+
+    | ECase (variant, handlers) ->
+      let fst (x, _) = x in
+      let keys m = StrSet.of_seq @@ Seq.map fst @@ StrMap.to_seq m in
+      let handler_ctors = keys handlers in
+      begin match aux ctx variant with
+      | (TyVariant bindings) as variant_ty ->
+          let variant_ctors = keys bindings in
+          if handler_ctors <> variant_ctors then
+            raise (Expected_Variant_Type_W_Ctors(handler_ctors, variant_ty))
+          else
+            begin match StrMap.bindings handlers with
+              | [] -> 
+                (*could have a better bottom here probably*)
+                variant_ty
+              | (ctor_name, handler) :: tl ->
+                let elab ctor_name handler = 
+                  let binding_ty = StrMap.find ctor_name bindings in
+                  let ctx = StrMap.add ctor_name binding_ty ctx in
+                  aux ctx handler
+                in
+                let expected_ty = elab ctor_name handler in
+                let check name handler =
+                  let ty = elab name handler in
+                  if not (ty_eq ty expected_ty) then
+                    raise (Type_Mismatch(expected_ty, ty))
+                  else
+                    ()
+                in begin
+                  StrMap.iter check handlers;
+                  expected_ty
+                end
+            end
+      | other ->
+        raise (Expected_Variant_Type_W_Ctors(handler_ctors, other))
+      end
   in
   validate_no_shadow e;
-  aux Ctx.empty e
+  aux StrMap.empty e
 
  
 let assert_ty e expected_ty =
@@ -194,3 +262,19 @@ let _ =
   assert_ty record (TyRecord (map_of_pairs [("a", TyBool); ("b", TyUnit); ("c", TyBool)]));
   assert_ty (ERecordProj(record, "a")) TyBool;
   assert_ty (ERecordProj(record, "b")) TyUnit;
+  let variant = EVariant("a", ETrue, TyVariant(map_of_pairs [("a", TyBool); ("b", TyUnit)])) in
+  let handlers = map_of_pairs [
+    ("a", EIf(EVar"a", EUnit, EUnit));
+    ("b", EVar"b")
+  ] in
+  let case = ECase(variant, handlers) in
+  assert_ty case TyUnit;
+  let extra_handlers = StrMap.add "c"  EUnit handlers in
+  let case_with_extra = ECase(variant, extra_handlers) in
+  begin match type_of case_with_extra with
+    | exception (Expected_Variant_Type_W_Ctors _ ) -> ()
+    | _ ->
+      failwith "expected exception"
+  end;
+  let bad_handlers = StrMap.add "b" ETrue handlers in
+  check_fails_ty_mismatch (ECase(variant, bad_handlers))
